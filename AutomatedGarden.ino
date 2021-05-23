3f48ab8c8f4f139c830ba1f9f34957b895bd7eee
Chris Kirilov <kristiyan.kkirilov@gmail.com>
2021-05-23 23:19:04 +0300

    Add files via upload

--- AutomatedGarden.ino
+++ AutomatedGarden.ino
@@ -1,3 +1,9 @@
+#include <FastLED.h>
+
+const int LED_PIN = 7;
+const int NUM_LEDS = 10;
+CRGB leds[NUM_LEDS];
+
 const int tempPin = A0;
 int tempInput;
 double temp;
@@ -5,40 +11,52 @@ double temp;
 const int lightPin = A1;
 int lightInput;
 
-const int humidityPin = A2; 
-int humidityValue; 
+const int humidityPin = A2;
+int humidityValue;
 
-const int motorPin = 8;
- 
+const int motorPin = 9;
+
 void setup() {
   Serial.begin(9600);
   pinMode(motorPin, OUTPUT);
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
 }
 void loop() {
   tempInput = analogRead(tempPin);
-  float humidityValue = analogRead(humidityPin); 
-  float lightInput = analogRead(lightPin); 
-  
- //converting that reading to voltage
- double voltage = tempInput * 5.0;
- voltage /= 1024; 
+  float humidityValue = analogRead(humidityPin);
+  float lightInput = analogRead(lightPin);
 
- float temperatureC = (voltage - 0.5) * 100 ;
- /*
- converting from 10 mv per degree with 500 mV offset
- to degrees ((voltage - 500mV) times 100)]
- */
+  //converting that reading to voltage
+  double voltage = tempInput * 5.0;
+  voltage /= 1024;
+
+  float temperatureC = (voltage - 0.5) * 100 ;
 
   humidityValue = humidityValue - 1023;
   humidityValue = abs(humidityValue);
+  humidityValue = map(humidityValue, 0, 1023, 0, 100);
 
   lightInput = analogRead(lightPin);
+  lightInput = map(lightInput, 0, 1023, 0, 100);
 
-  if(humidityValue < 20) {
-     digitalWrite(motorPin, HIGH); 
+  if (humidityValue < humidityPeak) {
+    digitalWrite(motorPin, HIGH);
   }
   else {
-     digitalWrite(motorPin, LOW);
+    digitalWrite(motorPin, LOW);
+  }
+
+  if (lightInput < lightPeak) {
+    for (int i = 0; i < 10; i++) {
+      leds[i] = CRGB(255, 249, 253);
+      FastLED.show();
+    }
+  }
+  else {
+    for (int i = 0; i < 10; i++) {
+      leds[i] = CRGB::Black;
+      FastLED.show();
+    }
   }
 
   Serial.print(temperatureC);
@@ -46,7 +64,7 @@ void loop() {
   Serial.print(lightInput);
   Serial.print(" ");
   Serial.print(humidityValue);
-  
-  delay(10000);
-  
+  delay(5000);
+
+
 }
